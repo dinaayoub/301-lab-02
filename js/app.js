273b2883f3b1fd74f2c879362d70650b0c259d4e
Dina Ayoub <dinaayoub@hotmail.com>
2020-10-07 16:29:17 -0700

    feature 2 completed

--- js/app.js
+++ js/app.js
@@ -1,43 +1,7 @@
-'use strict'
+/*'use strict'
 
-let $paginationButton = $('#pagination');
-let $dropDownMenu = $('select');
 
-$paginationButton.on('click', () => {
-  if ($paginationButton.attr('value') === 'Next Page'){
-    $paginationButton.attr('value', 'Previous Page');
-    $('section').not('.photo-template').remove();
-    $('option').not(':first-child').remove();
-    $.ajax('data/page-2.json', ajaxSettings)
-      .then(data => {
-        data.forEach(item => {
-          let photo = new Photo(item);
-          photo.render();
-          let $option = $(`<option>${item.title}</option>`).attr('value', item.keyword);
-          // $option.attr('class', item.keyword);
-          $dropDownMenu.append($option);
-        });
-        $('section').not('.photo-template').show();
-      })
-    // $('#myDropDown option').val('default').change();
-  } else {
-    $paginationButton.attr('value', 'Next Page');
-    $('section').not('.photo-template').remove();
-    $('option').not(':first-child').remove();
-    $.ajax('data/page-1.json', ajaxSettings)
-      .then(data => {
-        data.forEach(item => {
-          let photo = new Photo(item);
-          photo.render();
-          let $option = $(`<option>${item.title}</option>`).attr('value', item.keyword);
-          // $option.attr('class', item.keyword);
-          $dropDownMenu.append($option);
-        });
-        $('section').not('.photo-template').show();
-      })
-    // $('#myDropDown option').val('default').change();
-  }
-})
+
 
 function Photo(item) {
   this.title = item.title;
@@ -73,14 +37,85 @@ $.ajax('data/page-1.json', ajaxSettings)
     });
   })
 
+
+*/
+
+'use strict';
+let $paginationButton = $('#pagination');
+let $dropDownMenu = $('select');
+let templateId = '.photo-template';
+let photos = [];
+
+function Photo(rawDataObject) {
+  for (let key in rawDataObject) {
+    //assign the property name as our object's property name
+    //then assign that property's value to the raw object data's value
+    //object[key] is a way of doing object.key when all you have is a string.
+    this[key] = rawDataObject[key];
+  }
+}
+
+//"this" means the object that we are within.
+
+Photo.prototype.toHtml = function () {
+  //1. grab the template from our html document
+  let template = $(templateId).html();
+
+  //2. use Mustache to render the new html by merging it with data
+  let html = Mustache.render(template, this)
+
+  //3. return our html so it can be put on the page
+  return html;
+};
+
+
+page1.forEach(item => {
+  photos.push(new Photo(item));
+  let $option = $(`<option>${item.title}</option>`).attr('value', item.keyword);
+  $dropDownMenu.append($option);
+});
+
+photos.forEach(photo => {
+  $('#gallery').append(photo.toHtml())
+});
+
+$paginationButton.on('click', () => {
+  if ($paginationButton.attr('value') === 'Next Page') {
+    photos.splice(0, photos.length);
+    $paginationButton.attr('value', 'Previous Page');
+    $('section').remove();
+    $('option').not(':first-child').remove();
+    page2.forEach(item => {
+      photos.push(new Photo(item));
+      let $option = $(`<option>${item.title}</option>`).attr('value', item.keyword);
+      $dropDownMenu.append($option);
+    });
+    photos.forEach(photo => {
+      $('#gallery').append(photo.toHtml())
+    });
+  } else {
+    photos.splice(0, photos.length);
+    $paginationButton.attr('value', 'Next Page');
+    $('section').remove();
+    $('option').not(':first-child').remove();
+    page1.forEach(item => {
+      photos.push(new Photo(item));
+      let $option = $(`<option>${item.title}</option>`).attr('value', item.keyword);
+      $dropDownMenu.append($option);
+    });
+    photos.forEach(photo => {
+      $('#gallery').append(photo.toHtml())
+    });
+  }
+});
+
 $dropDownMenu.on('change', function () {
   //Code for filtering images here
   let $sections = $('section');
   $sections.hide();
   let title = $dropDownMenu[0][$dropDownMenu[0].selectedIndex].value;
-  console.log(title);
-  if (title === 'default'){
-    $('section').not('.photo-template').show();
+  if (title === 'default') {
+    $('section').show();
   } else {
     $(`.${title}`).show();
   }
